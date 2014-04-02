67915d3302357583d98ad4590392f6ba4c969e33
Morgan Evans <morganrevans@gmail.com>
2014-04-02 13:42:05 -0400

    Built send_message method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,5 +6,20 @@ class Contact < ActiveRecord::Base
     where("strftime('%d', birthday) = ? AND strftime('%m', birthday) = ?", Date.today.strftime('%d'), Date.today.strftime('%m'))
     
   end
+
+  def send_message(message)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    from = "+19172424924"
+
+    client.account.messages.create(
+      :from => from,
+      :to => self.phone_number,
+      :body => message
+    )
+    #TODO "Sent message to #{name}"
+  end
 end
   
