36739114c182c18efe9786264d597eb28857c85f
Boyoung Kwon <boyoung.kwon@gmail.com>
2014-04-06 12:05:57 -0400

    create postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20140404142403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "contacts", force: true do |t|
     t.string   "nick_name"
     t.string   "first_name"
